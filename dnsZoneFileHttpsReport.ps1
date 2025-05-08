e58252dd24c1ecb1bb3bcfc35f96716a2ee3d3d5
itm8certrenew <certrenew@itm8.com>
2025-05-08 10:46:08 +0200

    Update dnsZoneFileHttpsReport.ps1

--- dnsZoneFileHttpsReport.ps1
+++ dnsZoneFileHttpsReport.ps1
@@ -26,7 +26,7 @@ $ScriptTerminationSleep = 30;
 #
 ### Script
 $ThisDomain = $null
-$SupportedOS = IF (!((((Get-WmiObject -class Win32_OperatingSystem).Caption) -notlike "*Server 2016*") -and ((([Environment]::OSVersion).version) -ge [Version]‘10.0.0.0’) )) {
+$SupportedOS = IF (!((((Get-WmiObject -class Win32_OperatingSystem).Caption) -notlike "*Server 2016*") -and ((([Environment]::OSVersion).version) -ge [Version]"10.0.0.0") )) {
   Write-Host "`n  Os is not supported to run from Github!`n  -- Run a local version of this script --`n`n  Script will terminate!`n";
   Sleep $ScriptTerminationSleep;
   Break;};
